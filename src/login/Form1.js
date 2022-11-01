6534b784302264b26ae2148ef6db2db1d3f5217b
RajaRanjithkumar17 <rajaranjithkumar17.com>
2022-11-01 12:38:39 +0530

    my commit

--- src/login/Form1.js
+++ src/login/Form1.js
@@ -7,25 +7,25 @@ import data from '../Credentials/Userdetail.json'
 const Form1 = () => {
   const navigate = useNavigate();
 
-  const [userName , setUserName]= useState("ramm")
-  const [userPwd ,setUserPwd]= useState(1234)
+  const [userName , setUserName]= useState([])
+  const [userPwds ,setUserPwd]= useState([])
 
 
 
-  const HandelSub = ()=>{
-    //sub.preventDefault();
+  const HandelSub = (sub)=>{
+    sub.preventDefault();
 
-    var detail =  data.map((index)=>{ return(index.userName)  })
+    var detail =  data.map((index)=>{ return(index.usersName)  })
      
      
     var ind = detail.indexOf(userName);
     console.log(ind);
 
     var pwddetail =  data.map((index)=>{ return(index.password)})
-    var inde = pwddetail.indexOf(userPwd);
+    var inde = pwddetail.indexOf(userPwds);
     console.log(inde);
    
-    console.log("sub",detail,pwddetail);
+    console.log("sub",detail,pwddetail,userName,userPwds);
  
     if (ind===inde){                                          
 
@@ -46,14 +46,14 @@ const Form1 = () => {
 
 const handelName = (nam)=>{
   console.log("nam",nam);
-  if (nam.target.value==="userName") {
+  if (nam.target.name==="userName") {
    setUserName(nam.target.value) 
   }
 
 } 
 const handelPwd = (pwd)=>{
   console.log("nam",pwd);
-  if (pwd.target.value==="userpwd") {
+  if (pwd.target.name==="userpwd") {
     setUserPwd(pwd.target.value) 
   }
  
@@ -66,7 +66,7 @@ return (
       <form onSubmit={HandelSub}>
        
         <input onChange={handelName} name="userName" placeholder='User Name' value={userName}/>
-        <input  onChange={handelPwd} type={"password"} name="userpwd" placeholder='Password' value={userPwd}/>
+        <input  onChange={handelPwd} type={"password"} name="userpwd" placeholder='Password' value={userPwds}/>
         <input type={"submit"} id="submit"/>
       </form>
 
